a9cfea4f8cb281461480f601fa2aec0959feee6c
Alexander <pawsanie@gmail.com>
2026-08-04 15:52:56 +0500

    Update 1.2.0 - Windows -PowerShell: Windows.old devnul update

--- Scripts/Windows/windows_old_devnull.ps1
+++ Scripts/Windows/windows_old_devnull.ps1
@@ -41,42 +41,90 @@ function Remove-Directory {
         -Force `
         -ErrorAction SilentlyContinue `
         | ForEach-Object {
+
             if ($_.PSIsContainer) {
                 Remove-Directory $_.FullName
             }
+
             else {
                 $file = $_.FullName
 
-                Write-Host "Updating '$file' file permissions"
-                icacls.exe $file /grant '*S-1-5-32-544:F' /C | Out-Null
+                Write-Host `
+                    "Updating file permissions...`n" `
+                    "File path: '$file'" `
+                    -ForegroundColor White
+                icacls.exe $file `
+                    /grant '*S-1-5-32-544:F' `
+                    /C `
+                    | Out-Null
 
+               Write-Host `
+                    "Attempt to delete file...`n" `
+                    "File path: '$file'" `
+                        -ForegroundColor White
                 try {
-                    Write-Host "Attempt to delete '$file' file..."
                     Remove-Item `
                         -LiteralPath $file `
                         -Force `
-                        -ErrorAction Stop
+                        -ErrorAction Stop `
+                        -Confirm:$false
+                    Write-Host `
+                        "File deletion successful.`n" `
+                        "File path: '$file'" `
+                        -ForegroundColor White
                 }
+
                 catch {
-                    Write-Warning "Failed to delete: $file"
+                    Write-Host `
+                        "Failed to delete file!`n" `
+                        "File path: '$file'" `
+                        -ForegroundColor Yellow
                 }
+
             }
+
         }
 
     try {
-        Write-Host "Attempt to delete directory '$Directory'..."
+        Write-Host `
+            "Attempt to delete directory...`n" `
+             "Directory path: '$Directory'"
         Remove-Item `
             -LiteralPath $Directory `
             -Force `
-            -ErrorAction Stop
+            -ErrorAction Stop `
+            -Confirm:$false
     }
     catch {
-        Write-Warning "Failed to delete directory: $Directory"
+        Write-Host `
+            "Failed to delete directory!`n" `
+            "Directory path: '$Directory'" `
+            -ForegroundColor Yellow
     }
 }
 
 # Devnull "Windows.old":
-if (Test-Path $Path) {
-    Remove-Directory $Path
-    Write-Host "Windows.old was deleted."
+function Remove-WindowsOLD {
+    if (Test-Path $Path) {
+        Remove-Directory $Path
+
+        if (
+            -not (Test-Path $Path)
+        ) {
+            Write-Host "Windows.old deleted successfully." `
+                -ForegroundColor Green
+        }
+        else {
+            Write-Error "Failed to delete the Windows.old completely!"
+        }
+    }
+
+    else {
+        Write-Host `
+            "Unable to locate Windows.old!`n" `
+            "Directory path: '$Path'" `
+            -ForegroundColor Blue
+    }
 }
+
+Remove-WindowsOLD
