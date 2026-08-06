58ac16d70324181174b86d14e8e114ecfaba497f
Alexander <pawsanie@gmail.com>
2026-08-06 12:14:33 +0500

    Update 1.11.0 - Windows - PowerShell: Windows.old devnul update

--- Scripts/Windows/windows_old_devnull.ps1
+++ Scripts/Windows/windows_old_devnull.ps1
@@ -28,137 +28,233 @@ if (
 }
 
 # Path settings:
-$Path = 'C:\Windows.old'
+$TargetPath = 'C:\Windows.old'
+$AdministratorsPermissions = '*S-1-5-32-544:F'
 
-# Recursive devnull directory:
-function Remove-Directory {
+# Workers srttings:
+$PermissibleLoad = 2 / 3
+$Flows = [int](
+    [Environment]::ProcessorCount * $PermissibleLoad
+)
+
+
+<#
+.SYNOPSIS
+Get path massive for warkers.
+
+.PARAMETER Path
+Path to Windows.old directory.
+
+.OUTPUTS
+System.IO.DirectoryInfo pahs massive.
+#>
+function Get-File-Tree {
     param (
-        [string]$Directory
+        [string]$Path
     )
 
-    Get-ChildItem `
-        -LiteralPath $Directory `
-        -Force `
-        -ErrorAction SilentlyContinue `
-        | ForEach-Object {
+    Write-Host `
+        "Attempting to collect paths to the contents of the 'Windows.old' directory.`n" `
+        "Directory path: '$Path'" `
+        -ForegroundColor Blue
 
-            if ($_.PSIsContainer) {
-                $directory = $_.FullName
-                Write-Host `
-                    "Subfolder found: '$_'`n" `
-                    "Directory path: '$directory'" `
-                    -ForegroundColor Blue
-
-                Write-Host `
-                    "Updating subfolder permissions...`n" `
-                    "Directory path: '$directory'" `
-                    -ForegroundColor White
-                takeown.exe `
-                    /F "$directory" `
-                    | Out-Null
-                icacls.exe $directory `
-                    /grant '*S-1-5-32-544:F' `
-                    /C `
-                    | Out-Null
-
-                Write-Host `
-                    "Attempting to process nested content...`n" `
-                    "Directory path: '$directory'" `
-                    -ForegroundColor White
-                Remove-Directory $directory
-            }
-
-            else {
-                $file = $_.FullName
-
-                Write-Host `
-                    "Updating file permissions...`n" `
-                    "File path: '$file'" `
-                    -ForegroundColor White
-                takeown.exe `
-                    /F "$file" `
-                    | Out-Null
-                icacls.exe $file `
-                    /grant '*S-1-5-32-544:F' `
-                    /C `
-                    | Out-Null
-
-               Write-Host `
-                    "Attempt to delete file...`n" `
-                    "File path: '$file'" `
-                        -ForegroundColor White
-                try {
-                    Remove-Item `
-                        -LiteralPath $file `
-                        -Force `
-                        -ErrorAction Stop `
-                        -Confirm:$false
-                    Write-Host `
-                        "File deletion successful.`n" `
-                        "File path: '$file'" `
-                        -ForegroundColor Green
-                }
-
-                catch {
-                    Write-Host `
-                        "Failed to delete file!`n" `
-                        "File path: '$file'" `
-                        -ForegroundColor Yellow
-                }
-
-            }
-
-        }
-
-    try {
-        Write-Host `
-            "Attempt to delete directory...`n" `
-            "Directory path: '$Directory'" `
-            -ForegroundColor White
-
-        if (
-            @(
-                Get-ChildItem `
-                    -LiteralPath $Directory `
-                    -Force `
-                    -ErrorAction SilentlyContinue
-            ).Count -eq 0
-        ) {
-            Remove-Item `
-            -LiteralPath $Directory `
+     @(
+        Get-ChildItem `
+            -LiteralPath $Path `
             -Force `
-            -ErrorAction Stop `
-            -Confirm:$false
-
-            Write-Host `
-                "Directory deletion successful.`n" `
-                "Directory path: '$Directory'" `
-                -ForegroundColor Green
-        }
-        else {
-            Write-Host `
-            "Failed to delete directory!`n" `
-            "Folder is not empty: failed to pre-delete all files.`n" `
-            "Directory path: '$Directory'" `
-            -ForegroundColor Yellow
-        }
-
-    }
-    catch {
-        Write-Host `
-            "Failed to delete directory!`n" `
-            "Directory path: '$Directory'" `
-            -ForegroundColor Yellow
-    }
+            -Recurse `
+            -Directory `
+            -ErrorAction SilentlyContinue `
+            | Sort-Object FullName `
+                -Descending
+    )
 }
 
-# Devnull "Windows.old":
+<#
+.SYNOPSIS
+
+.PARAMETER Path
+Path to file or directory.
+#>
+function Remove-File-System-Item {
+    param (
+        [string]$Path
+    )
+
+    Remove-Item `
+        -LiteralPath $Path `
+        -Force `
+        -ErrorAction Stop `
+        -Confirm:$false
+
+}
+
+<#
+.SYNOPSIS
+Takes ownership of the file system item
+and grants Full Control permissions to the Administrators group.
+
+.PARAMETER Path
+Path to file or directory.
+#>
+function Grant-Permissions {
+    param (
+        [string]$Path
+    )
+
+    Write-Host `
+        "Updating file system item permissions...`n" `
+        "Item path: '$Path'" `
+        -ForegroundColor White
+
+    takeown.exe `
+        /F "$Path" `
+        | Out-Null
+
+    icacls.exe $Path `
+        /grant $AdministratorsPermissions `
+        /C `
+        | Out-Null
+
+    Write-Host `
+        "File system item permissions successfully updated.`n" `
+        "Item path: '$Path'" `
+        -ForegroundColor White
+
+    }
+
+<#
+.SYNOPSIS
+Deletes the file system item within the thread worker.
+
+.PARAMETER Path
+Path to file or directory.
+#>
+function Task {
+    param (
+        [string]$Path
+    )
+
+    Write-Host `
+        "Attempt to delete file system item...`n" `
+        "Item path: '$file'" `
+        -ForegroundColor White
+
+    try {
+        try {
+            Remove-File-System-Item `
+                -Path $Path
+        }
+        catch {
+            Write-Host `
+                "Access denied!`n" `
+                "Ownership and permissions must be acquired for the file system item.!`n" `
+                "Item path: '$file'" `
+                -ForegroundColor Yellow
+
+            Grant-Permissions `
+                -Path $Path
+
+            Remove-File-System-Item `
+                -Path $Path
+        }
+
+        Write-Host `
+            "File system item deletion successful.`n" `
+            "Item path: '$Path'" `
+            -ForegroundColor Green
+    }
+
+    catch {
+        Write-Host `
+            "Failed to delete file system item!`n" `
+            "Item path: '$file'" `
+            -ForegroundColor Red
+    }
+
+}
+
+<#
+.SYNOPSIS
+Create and run multi thread tasks.
+
+.PARAMETER Paths
+System.IO.DirectoryInfo pahs massive from Get-File-Tree.
+#>
+function Start-Tasks {
+    param (
+        [massive][System.IO.DirectoryInfo]$Paths
+    )
+    # Multy thred Runspace ScriptBlock:
+    $Worker = {
+        param(
+            [string]$Task,
+            [string]$Path
+        )
+
+        Invoke-Expression $Task
+        Task $Path
+    }
+
+    # Multythreds settings:
+    $Pool = [RunspaceFactory]::CreateRunspacePool(
+            1,
+            $Flows
+    )
+
+    # Run tasks:
+    $Pool.Open()
+
+    $PowerShellTasks = foreach ($Path in $Paths) {
+
+        $PowerShellWorker = [PowerShell]::Create()
+        $PowerShellWorker.RunspacePool = $Pool
+
+        $PowerShellWorker.AddScript($Worker) `
+            | Out-Null
+        $PowerShellWorker.AddArgument(
+                ${function:Task}.ToString()
+        ) `
+            | Out-Null
+        $PowerShellWorker.AddArgument($Path) `
+            | Out-Null
+        $Handle = $PowerShellWorker.BeginInvoke()
+
+        [PowerShellCustomObject]@{
+            PowerShell = $PowerShellWorker
+            Handle     = $Handle
+            Path       = $Path
+        }
+
+    }
+
+    # Awaite tasks finish:
+    foreach ($PSTask in $PowerShellTasks) {
+        try {
+            $PSTask.PowerShell.EndInvoke(
+                    $PSTask.Handle
+            )
+        }
+        finally {
+            $PSTask.PowerShell.Dispose()
+        }
+    }
+
+    $Pool.Close()
+    $Pool.Dispose()
+
+}
+
+<#
+.SYNOPSIS
+Devnull "Windows.old"
+#>
 function Remove-WindowsOLD {
-    if (Test-Path $Path) {
+    if (Test-Path $TargetPath) {
         Remove-Directory $Path
 
         if (
-            -not (Test-Path $Path)
+            -not (Test-Path $TargetPath)
         ) {
             Write-Host "Windows.old deleted successfully." `
                 -ForegroundColor Green
@@ -171,13 +267,14 @@ function Remove-WindowsOLD {
     else {
         Write-Host `
             "Unable to locate Windows.old!`n" `
-            "Directory path: '$Path'" `
+            "Directory path: '$TargetPath'" `
             -ForegroundColor Blue
     }
 
     Write-Host `
         "Windows.old devnull Scenario completed." `
         -ForegroundColor Blue
+
 }
 
 Remove-WindowsOLD
