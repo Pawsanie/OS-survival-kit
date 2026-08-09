32cd78f4d75fdfb25639cd88595cf4aab918849f
Alexander <pawsanie@gmail.com>
2026-08-09 23:06:25 +0500

    Update 2.0.0 - Windows - PowerShell: WER_ReportArchive_grant_read_access

---
+++ Scripts/Windows/WER_ReportArchive_grant_read_access.ps1
@@ -0,0 +1,59 @@
+#!/usr/bin/env pwsh
+#requires -Version 5.1
+# The script grants all accounts in the Administrators group permissions to read all WER reports.
+
+<#
+Unix Root user equivalent for scripts:
+Opens a new PowerShell window with elevated privileges
+and executes the entire subsequent script in it.
+#>
+if (
+    -not (
+        [Security.Principal.WindowsPrincipal] `
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole(
+            [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    Start-Process powershell `
+        -Verb RunAs `
+        -ArgumentList @(
+            '-NoProfile',
+            '-ExecutionPolicy',
+            'Bypass',
+            '-File',
+            $PSCommandPath
+    )
+    exit
+}
+
+Get-ChildItem `
+    -LiteralPath 'C:\ProgramData\Microsoft\Windows\WER\ReportArchive' `
+    -Force `
+    -Directory `
+    -ErrorAction SilentlyContinue `
+    | ForEach-Object {
+
+        $ReportName = $_
+        $ReportPath = $ReportName.FullName
+
+        takeown.exe `
+            /F $ReportPath `
+            /R `
+            /A `
+            /D Y `
+            | Out-Null
+
+        icacls.exe `
+            $ReportPath `
+            /grant "$($env:USERDOMAIN)\$($env:USERNAME):(OI)(CI)F" `
+            /setintegritylevel M `
+            /T `
+            /C `
+            | Out-Null
+
+        Write-Host `
+            "Permissions updated: $ReportName" `
+            -ForegroundColor Green
+
+    }
