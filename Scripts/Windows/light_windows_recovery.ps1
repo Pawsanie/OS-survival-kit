99838d8fb9577b1d2ca9d3d8a26e9db2788a1542
Alexander <pawsanie@gmail.com>
2026-08-11 00:05:02 +0500

    Update 3.2.0 - Windows - PowerShell: light_windows_recovery update

--- Scripts/Windows/light_windows_recovery.ps1
+++ Scripts/Windows/light_windows_recovery.ps1
@@ -1,6 +1,31 @@
 #!/usr/bin/env pwsh
 #requires -Version 5.1
 
+<#
+Unix Root user equivalent for scripts:
+Opens a new PowerShell window with elevated privileges
+and executes the entire subsequent script in it.
+#>
+if (
+    -not (
+        [Security.Principal.WindowsPrincipal] `
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole(
+            [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    Start-Process powershell `
+        -Verb RunAs `
+        -ArgumentList @(
+            '-NoProfile',
+            '-ExecutionPolicy',
+            'Bypass',
+            '-File',
+            $PSCommandPath
+    )
+    exit
+}
+
 function Clear-Windows-Update-Cache {
 
     $windowsUpdateServices = @(
@@ -16,7 +41,7 @@ function Clear-Windows-Update-Cache {
     Remove-Item "$env:windir\SoftwareDistribution\*" `
         -Recurse `
         -Force
-    
+
     Remove-Item "$env:windir\System32\catroot2\*" `
         -Recurse `
         -Force
@@ -40,12 +65,16 @@ function Start-Recovery-Trick {
 function Main {
 
     try {
-        Clear-Windows-Update-Сache `
+        Clear-Windows-Update-Cache `
             -ErrorAction Stop
     }
     catch {
+        Write-Error $_
         return
     }
     Start-Recovery-Trick
 
 }
+
+# Entry point:
+Main
