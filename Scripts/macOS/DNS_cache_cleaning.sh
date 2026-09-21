4864692a5ed7893c3bd752a659246139703debcc
Alexander <pawsanie@gmail.com>
2026-09-22 02:46:08 +0500

    Update 11.0.1 - macOS - Bash: DNS cache cleaning

--- Scripts/macOS/DNS_cache_cleaning.sh
+++ Scripts/macOS/DNS_cache_cleaning.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 # The script clears the DNS cache.
 
+printf '\033[34mmacOS DNS cache clearing script has been launched.\033[0m\n'
+
 sudo dscacheutil \
   -flushcache
 
 sudo killall \
   -HUP mDNSResponder
+
+printf '\033[34mmacOSs DNS cache clearing scenario completed.\033[0m\n'
